30b6a9add4817919df16a9507d6d8ba38ea00602
hipstar-08 <sonyjames9@gmail.com>
2021-11-21 09:05:01 +0530

    Delete LinkedList

--- linkedlist/com/sony/CustomLinkedList.java
+++ linkedlist/com/sony/CustomLinkedList.java
@@ -12,9 +12,9 @@ public class CustomLinkedList {
   }
 
   public void insertFirst(int value) {
-    Node node = new Node(value);
-    node.next = head;
-    head = node;
+    Node insertAtFirstNode = new Node(value);
+    insertAtFirstNode.next = head;
+    head = insertAtFirstNode;
 
     if (tail == null) {
       tail = head;
@@ -23,14 +23,88 @@ public class CustomLinkedList {
 
   }
 
+  public void insertLast(int value) {
+    if (tail == null)
+      insertFirst(value);
+
+    Node insertAtLastNode = new Node(value);
+    tail.next = insertAtLastNode;
+    tail = insertAtLastNode;
+    size += 1;
+  }
+
+  public void insertAtIndex(int value, int index) {
+    if (index == 0)
+      insertFirst(value);
+    else if (index == size)
+      insertLast(value);
+    else if (index > size)
+      throw new IndexOutOfBoundsException();
+    else {
+      Node temp = head;
+      for (int i = 1; i < index; i++) {
+        temp = temp.next;
+      }
+      Node nodeToBeInserted = new Node(value, temp.next);
+      temp.next = nodeToBeInserted;
+      size += 1;
+    }
+  }
+
+  public void deleteFirst() {
+    head = head.next;
+    if (head.next == null)
+      tail = null;
+    size -= 1;
+  }
+
+  public int deleteFirstAndReturnValue() {
+    int value = head.value;
+    head = head.next;
+
+    if (head.next == null)
+      tail = null;
+    size -= 1;
+    return value;
+  }
+
+  public void deleteLast() {
+    System.out.println("Size : " + size);
+    Node traverseUptoSecondLastNode = head;
+    for (int i = 0; i < size - 2; i++) {
+      traverseUptoSecondLastNode = traverseUptoSecondLastNode.next;
+      System.out.println(traverseUptoSecondLastNode.value);
+    }
+    tail = traverseUptoSecondLastNode;
+    tail.next = null;
+    // tail.next = null;
+    // while (true)
+    // System.out.println("test");
+    // tail = head.next;
+    // if (head.next == null)
+    // tail = null;
+  }
+
+  public int deleteLastAndReturnValue() {
+    int value = head.value;
+    head = head.next;
+
+    if (head.next == null)
+      tail = null;
+    size -= 1;
+    return value;
+  }
+
   public void display() {
     Node temp = head;
 
     while (temp != null) {
-      
-      System.out.print(temp.value + " -> ");
+      System.out.print(temp.value);
       temp = temp.next;
+      if (temp != null)
+        System.out.print(" -> ");
     }
+    System.out.println();
   }
 
   private class Node {
--- linkedlist/com/sony/Main.java
+++ linkedlist/com/sony/Main.java
@@ -8,9 +8,26 @@ public class Main {
     cll.insertFirst(2);
     cll.insertFirst(8);
     cll.insertFirst(1);
+    cll.insertFirst(4);
+    cll.insertFirst(7);
+    cll.insertFirst(10);
     cll.insertFirst(9);
 
+    cll.insertLast(77);
+    cll.insertAtIndex(55, 4);
+
     cll.display();
+    // cll.insertAtIndex(55, 20);
+
+    cll.deleteFirst();
+    cll.display();
+    int value = cll.deleteFirstAndReturnValue();
+    System.out.println("Value deleted : " + value);
+    cll.display();
+
+    cll.deleteLast();
+    cll.display();
+
   }
 
 }
